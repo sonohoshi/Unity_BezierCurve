b12a17446a3ffba1c246ee0215ef09df70e6ec1f
sonohoshi <yd111544@gmail.com>
2020-08-25 22:14:56 +0900

    Make Quadratic Bezier Curve
    Now Point move to follow Quadratic bezier curve.

---
+++ BezierCurveProject/Assets/Scripts/QuadraticBezier.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadraticBezier : MonoBehaviour
+{
+    public Transform[] Points;
+    public Transform[] QPoints;
+    public Transform BezierPoint;
+    
+    private LineRenderer lineRenderer;
+    private float time = 0f;
+    private float limit = 1f; 
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+    
+    void FixedUpdate()
+    {
+        time += Time.deltaTime;
+        if (time > limit) time = 0f;
+
+        for (int i = 0; i < Points.Length; i++)
+        {
+            lineRenderer.SetPosition(i,Points[i].position);
+        }
+
+        BezierPoint.position = Bezier(time, Points, QPoints);
+    }
+
+    private Vector3 Bezier(float t, Transform[] points, Transform[] qPoints)
+    {
+        qPoints[0].position = Vector3.Lerp(points[0].position, points[1].position, t);
+        qPoints[1].position = Vector3.Lerp(points[1].position, points[2].position, t);
+
+        return Vector3.Lerp(qPoints[0].position,qPoints[1].position, t);
+    }
+}
