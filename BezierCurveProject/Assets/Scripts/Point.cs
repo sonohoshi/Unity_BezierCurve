c9a40bcc424f621427be9d66196ee5fdd7ecfbcd
sonohoshi <yd111544@gmail.com>
2020-08-25 17:32:36 +0900

    Init Project
    Let's start!
    I implemented object move with mouse drag.

---
+++ BezierCurveProject/Assets/Scripts/Point.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour
+{
+    void OnMouseDrag() {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+        Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+        objectPos.z = transform.position.z;
+        transform.position = objectPos;
+    }
+}
